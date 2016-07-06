ad4940c82fe34d3631d9bde66dbffd71b1a17e80
danny song <danny.song.ga@gmail.com>
2016-07-06 23:06:24 +0900

    @sedman : leetcode_136_single_number#2

--- 001_single-number/sedman_single-number.cpp
+++ 001_single-number/sedman_single-number.cpp
@@ -1,30 +1,56 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
+#include <map>
 
-class Solution {
+class Solution_nlogn{
 public:
     int singleNumber(std::vector<int>& nums) {
       std::sort(nums.begin(), nums.end()); //quick sort
-      int n_pair = nums.size()/2;
-      int num_idx;
-      for(int i = 0; i < n_pair; ++i)
+      for(int idx = 0; idx < nums.size(); idx += 2)
       {
-        num_idx = i*2;
-        if(nums[num_idx] != nums[num_idx + 1])
+        if(nums[idx] != nums[idx + 1])
         {
-          return nums[num_idx];
+          return nums[idx];
         }  
       }
-
       return nums[nums.size() - 1];
     }
 };
 
+class Solution_n
+{
+public:
+  int singleNumber(std::vector<int>& nums) {
+    std::unordered_map<int, int> check_point;//hash table
+    for(auto num : nums)
+    {
+      check_point[num] += 1;
+    }
+    
+    for(auto point : check_point)
+    {
+      if(point.second == 1)
+      {
+        return point.first;
+      }
+    }
+    return -1;//not gonna happen. to prevent build error.
+  }  
+};
+
 int main(void)
 {
-  std::vector<int> nums {2,1,3,2,3,1};
-  Solution s;
+  std::vector<int> nums;
+  for(int i = 0; i < 5000000; ++i)
+  {
+    nums.push_back((i + 1)%9);
+  }
+
+  nums.push_back(9);
+  
+  Solution_n s;
   std::cout<<s.singleNumber(nums)<<"\n";
   return 0;
 }
